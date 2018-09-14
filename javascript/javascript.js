0f452a1ff01287f321427c75e0f168fb71e83381
Vitor Apolinário <vitorapoli@gmail.com>
2018-09-14 20:20:33 -0300

    one more commit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,12 @@
-function boasvindas() {
-	alert('Hello world!')
-}
+document.getElementById("controle-menu").onclick = function(){	
+	var menu=document.getElementsByClassName("menu-opcoes")[0];
+	var header=document.getElementsByClassName("c1")[0];
+
+	if(menu.style.display=='none'){
+		menu.style.display='block';
+		header.style.height='200px';
+	}else{
+		menu.style.display='none';
+		header.style.height='65px';
+	}
+};
