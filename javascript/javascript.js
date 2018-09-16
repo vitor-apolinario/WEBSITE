e23d3c10e02a78ebea462bb13a811e9bede5213a
Vitor Apolinário <vitorapoli@gmail.com>
2018-09-16 18:06:21 -0300

    domingo a noite

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,18 @@
+/*fecha o menu quando carrega a página*/
+window.onload = function(){
+	var menu=document.getElementsByClassName("menu-opcoes")[0];
+	var header=document.getElementsByClassName("c1")[0];
+
+	if(menu.style.display=='none'){
+		menu.style.display='block';
+		header.style.height='200px';
+	}else{
+		menu.style.display='none';
+		header.style.height='65px';
+	}
+}
+
+/*fecha/abre o menu de acordo com o click no hamburger*/
 document.getElementById("controle-menu").onclick = function(){	
 	var menu=document.getElementsByClassName("menu-opcoes")[0];
 	var header=document.getElementsByClassName("c1")[0];
