0f9d9bce123d8120137ccb56afa0a4755730669b
Vitor Apolinário <vitorapoli@gmail.com>
2018-09-17 22:28:46 -0300

    responsive/desktop

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,6 +3,10 @@ window.onload = function(){
 	var menu=document.getElementsByClassName("menu-opcoes")[0];
 	var header=document.getElementsByClassName("c1")[0];
 
+	//se estiver em desktop pula essa parte
+	if(screen.width > 500)
+		return;
+
 	if(menu.style.display=='none'){
 		menu.style.display='block';
 		header.style.height='200px';
